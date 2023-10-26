a4682d5acb75ca3fa53a9a6f36a1caf6dc2bc8d2
agent_tim <agent_tim@mail.ru>
2023-10-26 16:39:27 +0500

    работающая редакция

--- App.js
+++ App.js
@@ -1,46 +1,57 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
-  );
+function EmptyScreen() {
+    return <View />;
 }
+
+function Feed({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Feed Screen</Text>
+            <Button title="Go to Root" onPress={() => navigation.navigate('Root')} />
+            <Button
+                title="Go to Root, Profile"
+                onPress={() => navigation.navigate('Root', { screen: 'Profile' })}
+            />
+        </View>
+    );
+}
+
+function Home({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button title="Go to Feed" onPress={() => navigation.navigate('Feed')} />
+        </View>
+    );
+}
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function Root() {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Profile" component={EmptyScreen} />
+            <Stack.Screen name="Settings" component={EmptyScreen} />
+        </Drawer.Navigator>
+    );
+}
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Root" component={Root} />
+                <Stack.Screen name="Feed" component={Feed} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
